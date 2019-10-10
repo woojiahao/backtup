efe86c86669f678d5409bbb8a0f2325488194870
woojiahao <woojiahao@gmail.com>
2019-10-11 07:39:47 +0800

    Renamed Test command to Backtup

--- src/main/kotlin/com/github/woojiahao/Backtup.kt
+++ src/main/kotlin/com/github/woojiahao/Backtup.kt
@@ -1,18 +1,24 @@
 package com.github.woojiahao
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.prompt
 import com.github.ajalt.clikt.parameters.types.int
 
-class Test : CliktCommand() {
-  val count: Int by option(help = "Number of greetings").int().default(1)
-  val name: String by option(help = "The person to greet").prompt("Your name")
-
+class Backtup : CliktCommand() {
   override fun run() {
-    (1..count).forEach { _ -> echo("Hello $name") }
+    echo(getFormattedHelp())
   }
 }
 
-fun main(args: Array<String>) = Test().main(args)
+class List : CliktCommand() {
+  override fun run() {
+
+  }
+}
+
+fun main(args: Array<String>) = Backtup()
+  .subcommands(List())
+  .main(args)
