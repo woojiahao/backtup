7e401ac453561d23ad982513cd88ba07b1bd8fe4
woojiahao <woojiahao@gmail.com>
2019-10-11 12:05:25 +0800

    ConfigurationComponent can read/write to json

--- src/main/kotlin/com/github/woojiahao/models/ConfigurationComponent.kt
+++ src/main/kotlin/com/github/woojiahao/models/ConfigurationComponent.kt
@@ -2,12 +2,27 @@ package com.github.woojiahao.models
 
 import com.google.gson.JsonArray
 import com.google.gson.JsonObject
+import sun.security.krb5.Config
 
 class ConfigurationComponent(
   val name: String,
   val path: String,
   vararg val items: String
 ) {
+  companion object {
+    fun fromJson(jsonObject: JsonObject): List<ConfigurationComponent> {
+      val components = mutableListOf<ConfigurationComponent>()
+      jsonObject.entrySet().forEach {
+        val name = it.key
+        val path = it.value.asJsonObject["path"].asString
+        val items = it.value.asJsonObject["items"].asJsonArray.map { item -> item.asString }
+        val component = ConfigurationComponent(name, path, *items.toTypedArray())
+        components += component
+      }
+      return components
+    }
+  }
+
   fun toJson(): JsonObject {
     val componentInfo = JsonObject().apply {
       addProperty("path", path)
@@ -20,5 +35,12 @@ class ConfigurationComponent(
     return JsonObject().apply {
       add(name, componentInfo)
     }
+  }
+
+  override fun equals(other: Any?): Boolean {
+    if (other !is ConfigurationComponent) return false
+    return name == other.name &&
+      path == other.path &&
+      items.toList() == other.items.toList()
   }
 }
--- src/test/kotlin/com/github/woojiahao/models/ConfigurationComponentTest.kt
+++ src/test/kotlin/com/github/woojiahao/models/ConfigurationComponentTest.kt
@@ -16,6 +16,27 @@ class ConfigurationComponentTest {
     assertComponentJson("phone", "/run/media/")
   }
 
+  @Test
+  fun `fromJson converts JsonObject to ConfigurationComponent`() {
+    val json = createJson("default", "~", "Documents", "Pictures")
+    val configurationComponent = ConfigurationComponent.fromJson(json)
+    val expectedComponent = ConfigurationComponent("default", "~", "Documents", "Pictures")
+    assertEquals(expectedComponent, configurationComponent[0])
+  }
+
+  @Test
+  fun `fromJson converts JsonObject with multiple components to list of ConfigurationComponent`() {
+    val json = createJson("default", "~", "Documents", "Pictures")
+    val phoneJson = createInnerJson("/run/media", "Gallery")
+    json.add("phone", phoneJson)
+    val expectedComponents = listOf(
+      ConfigurationComponent("default", "~", "Documents", "Pictures"),
+      ConfigurationComponent("phone", "/run/media", "Gallery")
+    )
+    val configurationComponents = ConfigurationComponent.fromJson(json)
+    assertEquals(expectedComponents, configurationComponents)
+  }
+
   private fun assertComponentJson(name: String, path: String, vararg items: String) {
     val configurationComponent = ConfigurationComponent(name, path, *items)
     val componentJson = configurationComponent.toJson()
@@ -23,17 +44,17 @@ class ConfigurationComponentTest {
     assertEquals(expectedJson, componentJson)
   }
 
-  private fun createJson(name: String, path: String, vararg items: String): JsonObject {
-    val innerJson = JsonObject().apply {
+  private fun createJson(name: String, path: String, vararg items: String) =
+    JsonObject().apply {
+      add(name, createInnerJson(path, *items))
+    }
+
+  private fun createInnerJson(path: String, vararg items: String) =
+    JsonObject().apply {
       addProperty("path", path)
       val itemsArray = JsonArray().apply {
         items.forEach { add(it) }
       }
       add("items", itemsArray)
     }
-
-    return JsonObject().apply {
-      add(name, innerJson)
-    }
-  }
 }
