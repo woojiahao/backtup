045094902b4a8bb9945841a25c9c0c281bbdaad9
woojiahao <woojiahao@gmail.com>
2019-10-11 10:26:34 +0800

    Added fun to create .backup.json

--- src/main/kotlin/com/github/woojiahao/utility/ConfigurationUtility.kt
+++ src/main/kotlin/com/github/woojiahao/utility/ConfigurationUtility.kt
@@ -5,3 +5,5 @@ import java.io.File
 const val BACKUP_FILE_NAME = ".backup.json"
 
 fun hasBackupFile(root: String = "~") = File(path(root, BACKUP_FILE_NAME)).exists()
+
+fun createBackupFile(root: String = "~") = File(path(root, BACKUP_FILE_NAME)).createNewFile()
