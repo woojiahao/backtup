f1e965dc4ea0abe0d53421e3cd1afedfff7d7f6f
woojiahao <woojiahao@gmail.com>
2019-10-11 15:17:25 +0800

    Removed redundant default parameter for Status

--- src/main/kotlin/com/github/woojiahao/models/status/Status.kt
+++ src/main/kotlin/com/github/woojiahao/models/status/Status.kt
@@ -1,6 +1,6 @@
 package com.github.woojiahao.models.status
 
-sealed class Status<T>(default: T, val value: T = default) {
+sealed class Status<T>(val value: T) {
   class Fail<T>(default: T, vararg errorComponents: String): Status<T>(default) {
     val error = errorComponents.joinToString("\n")
   }
