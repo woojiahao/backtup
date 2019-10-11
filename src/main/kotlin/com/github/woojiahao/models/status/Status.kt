94d7cd6c2617dd6f7cf43c9a969a86c1c6f814ec
woojiahao <woojiahao@gmail.com>
2019-10-11 15:16:14 +0800

    Cleaned up Status class

--- src/main/kotlin/com/github/woojiahao/models/status/Status.kt
+++ src/main/kotlin/com/github/woojiahao/models/status/Status.kt
@@ -1,7 +1,7 @@
 package com.github.woojiahao.models.status
 
 sealed class Status<T>(default: T, val value: T = default) {
-  class Fail<T>(default: T, private vararg val errorComponents: String): Status<T>(default) {
+  class Fail<T>(default: T, vararg errorComponents: String): Status<T>(default) {
     val error = errorComponents.joinToString("\n")
   }
 
